ea7dfae991cecd4eddae77881a6fb56d9a66cb5a
Eselase-Noble <eselasenoble02@gmail.com>
2026-01-07 16:30:01 +0000

    Auto Commit: 2026-01-07 16:30:01

--- internal/projects/routes.go
+++ internal/projects/routes.go
@@ -20,6 +20,7 @@ func RegisterProjects(r chi.Router, db *pgxpool.Pool) {
 	r.Get("/projects/all", listProjects(db))
 }
 
+//Create a new project
 func createProject(db *pgxpool.Pool) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		userId := r.Context().Value("userId").(string)
@@ -36,5 +37,42 @@ func createProject(db *pgxpool.Pool) http.HandlerFunc {
 				 VALUES($1, $2, $3, $4, $5, $6, $7)`,
 			id, req.Name, req.Description, userId, time.Now(), userId, time.Now(),
 		)
+		if err != nil {
+			http.Error(w, "Error creating a new project", http.StatusInternalServerError)
+			return
+		}
+		w.WriteHeader(http.StatusCreated)
+	}
+}
+
+//List all the projects assigned to the logged user
+func listProjects(db *pgxpool.Pool) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		userId := r.Context().Value("userId").(string)
+
+		rows, err := db.Query(
+			r.Context(),
+			`SELECT projectId, projectName, description FROM projects 
+				WHERE createdBy = $1 OR projectId IN (SELECT projectId FROM project_members  WHERE userId = $1)`, userId,
+		)
+
+		if err != nil {
+			http.Error(w, "Error listing projects", http.StatusInternalServerError)
+			return
+		}
+		defer rows.Close()
+
+		var projects []map[string]string
+		for rows.Next() {
+			var projectId, projectName, description string
+			rows.Scan(&projectId, &projectName, &description)
+			projects = append(projects, map[string]string{
+				"projectId":   projectId,
+				"projectName": projectName,
+				"description": description,
+			})
+		}
+
+		json.NewEncoder(w).Encode(projects)
 	}
 }
