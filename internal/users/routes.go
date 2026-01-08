8c0dadf7f61e60d248fc40548fc44d4589a0e654
Eselase-Noble <eselasenoble02@gmail.com>
2026-01-08 15:00:01 +0000

    Auto Commit: 2026-01-08 15:00:01

--- internal/users/routes.go
+++ internal/users/routes.go
@@ -25,10 +25,10 @@ type userResponse struct {
 func RegisterUsers(r chi.Router, db *pgxpool.Pool, cfg *config.Config) {
 	// User management
 	r.Post("/users/create-user", createUser(db))
-	r.Put("/users/{userId}", updateUser(db))
+	r.Put("/users/update-user/{userId}", updateUser(db))
 	r.Delete("/users/{userId}", deleteUser(db))
 	r.Get("/users/all", getUsers(db))
-	r.Get("/users/{userId}", getUser(db))
+	r.Get("/users/get-user/{userId}", getUser(db))
 }
 
 func createUser(db *pgxpool.Pool) http.HandlerFunc {
